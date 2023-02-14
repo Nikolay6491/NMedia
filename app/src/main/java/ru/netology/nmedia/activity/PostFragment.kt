5990b0848c65858c2594d250a2ab135f54bb9658
Nikolay6491 <nikshustov@gmail.com>
2023-02-14 16:38:28 +0400

    Fragments, FragmentManager #2

---
+++ app/src/main/java/ru/netology/nmedia/activity/PostFragment.kt
@@ -0,0 +1,83 @@
+package ru.netology.nmedia.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.R
+import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostsAdapter
+import ru.netology.nmedia.databinding.FragmentPostBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class PostFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentPostBinding.inflate(inflater, container, false)
+
+        val viewModel by viewModels<PostViewModel>(ownerProducer = ::requireParentFragment)
+        PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+                findNavController().navigate(
+                    R.id.action_feedFragment_to_editPostFragment,
+                    Bundle().apply {
+                        textArg = post.content
+                    }
+                )
+            }
+
+            override fun onFavorite(post: Post) {
+                viewModel.favoriteById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                    type = "text/plain"
+                }
+
+                val shareIntent = Intent.createChooser(intent, getString(R.string.share))
+                startActivity(shareIntent)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun playVideo(post: Post) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(post.video))
+                startActivity(intent)
+            }
+        })
+
+        val newPostLauncher = registerForActivityResult(NewPostFragment.Contract) { result ->
+            result ?: return@registerForActivityResult
+            viewModel.changeContent(result)
+        }
+
+        viewModel.edited.observe(viewLifecycleOwner) {
+            if (it.id == 0L) {
+                return@observe
+            }
+            newPostLauncher.launch(it.content)
+        }
+
+        return binding.root
+
+
+    }
+
+}
