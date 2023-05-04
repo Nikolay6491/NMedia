bf3009d9bb9b977df2df6548a8630653763b7b79
Nikolay6491 <nikshustov@gmail.com>
2023-05-04 16:30:25 +0400

    Main Thread & Background #3

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -55,9 +55,14 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
     fun likesById(id: Long, likesByMe: Boolean) {
         thread {
             val newPost = repository.likes(id, likesByMe)
-            val old = _data.value?.posts.orEmpty()
-            val posts = listOf(newPost)+old
-            _data.postValue(FeedModel(posts=posts))
+            _data.postValue(
+                _data.value?.copy(posts = _data.value?.posts.orEmpty()
+                    .map {
+                        if(it.id != id) it
+                        else newPost
+                    }
+                )
+            )
         }
     }
 
